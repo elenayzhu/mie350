80fa6b89eea8e8880251665090b2a304162d9cb1
grace-ylin <grrace.lin@mail.utoronto.ca>
2026-03-07 16:52:48 -0500

    Update PartPickerApplication.java

--- pc-part-picker/src/main/java/com/team15/partpicker/PartPickerApplication.java
+++ pc-part-picker/src/main/java/com/team15/partpicker/PartPickerApplication.java
@@ -1,12 +1,17 @@
 package com.team15.partpicker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PartPickerApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PartPickerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(PartPickerApplication.class, args);
+        LOG.info("APPLICATION IS RUNNING");
     }
 }
